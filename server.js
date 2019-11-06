6d83b0423a5e8d1579732886aa2f74e8807187f0
TroddenSpade <parsasam1378@gmail.com>
2019-11-06 09:50:19 +0330

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const Telegraf = require("telegraf");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.command("oldschool", ctx => ctx.reply("Hello"));
+bot.command("modern", ({ reply }) => reply("Yo"));
+bot.command("hipster", Telegraf.reply("λ"));
+bot.launch();

24fc237f6a51709290d8c99d901e87d22cf03e86
TroddenSpade <parsasam1378@gmail.com>
2019-11-06 12:43:21 +0330

    Create User.js

---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  stuId: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  pass: {
+    type: String,
+    required: true
+  },
+  telId: {
+    type: String,
+    required: true,
+    unique: true
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };

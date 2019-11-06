f22ae22663a1227b5355820f78c0acebb055fa47
TroddenSpade <parsasam1378@gmail.com>
2019-11-06 19:18:34 +0330

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -15,6 +15,11 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
     unique: true
+  },
+  chatId: {
+    type: String,
+    required: true,
+    unique: true
   }
 });
 

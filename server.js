b6993e7e0c892a0f8b8f5079e524b8907f0cf9e4
TroddenSpade <parsasam1378@gmail.com>
2019-11-06 09:54:21 +0330

    env added

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const Telegraf = require("telegraf");
+const Markup = require("telegraf/markup");
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
 bot.command("oldschool", ctx => ctx.reply("Hello"));
 bot.command("modern", ({ reply }) => reply("Yo"));
 bot.command("hipster", Telegraf.reply("λ"));

7d5457aadd4868a6087f1eca10407e4165113ae2
TroddenSpade <parsasam1378@gmail.com>
2020-01-04 00:09:52 +0330

    Update get.js

--- src/function/get.js
+++ src/function/get.js
@@ -52,7 +52,9 @@ function __t0uchB0dy(b0dy) {
 
 function __get_pr1ce(b0dy, index) {
   let start =
-    b0dy.search(`this, 'userWeekReserves.selectedCount${index}', '`) + 42;
+    b0dy.search(`this, 'userWeekReserves.selectedCount${index}', '`) +
+    42 +
+    index / 10;
   let i;
   for (i = start; ; i++) {
     if (b0dy.charAt(i) === "'") break;
@@ -64,10 +66,20 @@ function __get_data(b0dy, index) {
   let time = b0dy.indexOf(`userWeekReserves[${index}].programDateTime"`);
   let proID = b0dy.indexOf(`userWeekReserves[${index}].programId"`);
   let type = b0dy.indexOf(`userWeekReserves[${index}].foodTypeId"`);
+  let price = b0dy.indexOf(`this, 'userWeekReserves.selectedCount${index}', '`);
+  let time_last = 38;
+  let proID_last = 32;
+  let type_last = 33;
+  let price_last = 43;
+  while (b0dy.charAt(time + time_last) != '"') time_last++;
+  while (b0dy.charAt(proID + proID_last) != '"') proID_last++;
+  while (b0dy.charAt(type + type_last) != '"') type_last++;
+  while (b0dy.charAt(price + price_last) != '"') price_last++;
+
   return {
-    programId: b0dy.substring(proID + 134, proID + 140),
-    time: b0dy.substring(time + 140, time + 153),
-    foodTypeId: b0dy.substring(type + 135, type + 138),
+    programId: b0dy.substring(proID + proID_last + 1, proID + proID_last + 7),
+    time: b0dy.substring(time + time_last + 1, time + time_last + 14),
+    foodTypeId: b0dy.substring(type + type_last + 1, type + type_last + 4),
     price: __get_pr1ce(b0dy, index),
     name: __get_F00d_nam3(b0dy, index)
   };
@@ -99,7 +111,7 @@ function __getP(name, priority) {
       return i;
     }
   }
-  return 30;
+  return 100;
 }
 
 module.exports = {
